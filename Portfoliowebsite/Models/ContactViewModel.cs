e20c4b2d67eefe202fae9ad133cafc4c4abf6c1e
Ye Htet Rohim <80707945+yehtet2006@users.noreply.github.com>
2026-03-06 21:53:16 +0100

    Implement contact form with validation and email sending functionality

---
+++ Portfoliowebsite/Models/ContactViewModel.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Portfoliowebsite.Models;
+
+public class ContactViewModel
+{
+    [Required(ErrorMessage = "E-mail is verplicht ") ]
+    [EmailAddress (ErrorMessage = "Ongeldig e-mailadres")]
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "Er moet een naam bevatten")]
+    [StringLength(100, ErrorMessage = "De naam mag niet langer zijn dan 100 tekens")]
+    public string Name { get; set; }
+    
+    [StringLength(150, ErrorMessage = "Het onderwerp mag niet langer zijn dan 150 tekens")]
+    [Required (ErrorMessage = "Er moet een onderwerp bevatten")]
+    public string Subject { get; set; }
+    
+    [Required(ErrorMessage = "Er moet een bericht bevatten")]
+    [StringLength(1000, ErrorMessage = "Het bericht mag niet langer zijn dan 1000 tekens")]
+    public string Message { get; set; }
+    
+    //honeypot veld
+    public string Website { get; set; }
+}
